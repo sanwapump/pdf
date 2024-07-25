9d96f4ea1b08b6e22fbc742ac8715b3db2eded4b
sanwapump <ishibe00@gmail.com>
2024-07-25 17:08:21 +0900

    Update viewer.mjs

--- viewer.mjs
+++ viewer.mjs
@@ -13029,7 +13029,6 @@ const PDFViewerApplication = {
     this.metadata = metadata;
     this._contentDispositionFilename ??= contentDispositionFilename;
     this._contentLength ??= contentLength;
-    console.log(`PDF ${pdfDocument.fingerprints[0]} [${info.PDFFormatVersion} ` + `${(info.Producer || "-").trim()} / ${(info.Creator || "-").trim()}] ` + `(PDF.js: ${version || "?"} [${build || "?"}])`);
     let pdfTitle = info.Title;
     const metadataTitle = metadata?.get("dc:title");
     if (metadataTitle) {
