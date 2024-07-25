3752d8fe6f6d0090449d87fdf192e8c62853ec51
sanwapump <ishibe00@gmail.com>
2024-07-25 19:04:35 +0900

    Update pdf.worker.mjs

--- pdf.worker.mjs
+++ pdf.worker.mjs
@@ -342,7 +342,7 @@ function info(msg) {
 }
 function warn(msg) {
   if (verbosity >= VerbosityLevel.WARNINGS) {
-    console.log(`Warning: ${msg}`);
+    //console.log(`Warning: ${msg}`); 24/07/25 by ki
   }
 }
 function unreachable(msg) {
